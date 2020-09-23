01e04554ce050469d13cd9959ed52b99fbead0da
Ethan Clark <clark.ethan.r@gmail.com>
2020-09-22 18:28:15 -0600

    added todo and footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import List from './components/todos/List';
+import TodoForm from './components/todos/TodoForm';
+import Footer from './components/todos/Footer';
+class App extends Component {
+  state = { todos: [
+    { id: 1, title: "Learn Rails", complete: true },
+    { id: 2, title: "Learn React", complete: false },
+    { id: 3, title: "Learn Router", complete: false },
+  ],
+    filter: 'All'
+  }
+  setFilter = (filter) => {
+    this.setState({ filter })
+  }
+  visibleItems = () => {
+    const { todos, filter } = this.state
+    switch(filter) {
+      case 'Active':
+        return todos.filter( t => !t.complete)
+      case 'Complete':
+        return todos.filter( t => t.complete)
+      default: 
+        return todos
+    }
+  }
+  // renderTodos = () => {
+  //   const { todos } = this.state
+  //   return todos.map( todo => {
+  //     return ( 
+  //       <li key={todo.id}>{todo.title}</li>
+  //     )
+  //   })
+  // }
+  getUniqId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+          //  { title: 'test', complete: false}
+  addTodo = (incomingTodo) => {
+    const { todos } = this.state
+    const newTodo = { id: this.getUniqId(), ...incomingTodo }
+    // const newTodo = { id: this.getUniqId(), title: incomingTodo.title, complete: incomingTodo.complete }
+    this.setState({ todos: [ ...todos, newTodo ]})
+  }
+  updateComplete = (id) => {
+    const { todos } = this.state 
+    this.setState({
+      todos: todos.map( t => {
+        if ( t.id === id ) {
+          return { ...t, complete: !t.complete }
+        }
+        return t
+      })
+    })
+  }
+  render() {
+    const { filter } = this.state
+    return(
+      <div>
+        <ul>
+          <Footer filter={filter} setFilter={this.setFilter} />
+          <TodoForm addTodo={this.addTodo} />
+          {/* { this.renderTodos() } */}
+          <List 
+            todos={this.visibleItems()} 
+            title='Learning List' 
+            updateComplete={this.updateComplete} 
+          />
+        </ul>
+      </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/conponents/todos/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Todo from './Todo';
+const List = ({ title, todos, updateComplete }) => (
+  <div>
+    <h2>{title}</h2>
+    <ul>
+                                       {/* title: t.title */}
+      { todos.map( t => <Todo key={t.id} {...t} updateComplete={updateComplete} />)}
+    </ul>
+  </div>
+)
+// {
+//   return(
+//   )
+// }
+export default List;
